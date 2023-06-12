2cdf033e7ab489028cc46ff08fae6dd32cd803f7
noyrosenbaum <noyrosen123@gmail.com>
2023-06-12 18:27:54 +0300

    updated

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -6,11 +6,7 @@ bool isPrime(int num)
 {
     if (num <= 1)
         return false;
-    if (num == 2)
-        return true;
-    if (num % 2 == 0)
-        return false;
-    for (int i = 3; i <= sqrt(num); i += 2)
+    for (int i = 2; i <= sqrt(num); i++)
     {
         if (num % i == 0)
             return false;
